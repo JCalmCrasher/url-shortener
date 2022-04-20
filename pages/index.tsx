39df05db7034bdf433f4de02eb838209223e9976
JoshX [MacBookPro] <joshua.osagie01@gmail.com>
2022-04-20 12:33:00 +0100

    init

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>jxly</title>
+        <meta name="description" content="url shortener" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main></main>
+    </div>
+  );
+};
+
+export default Home;
