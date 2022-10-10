f6005c5cf9a5057104d1519379b4439fa3b84c21
joshx <joshua.osagie01@gmail.com>
2022-10-10 09:41:17 +0100

    misc

--- src/pages/api/[id].ts
+++ src/pages/api/[id].ts
@@ -17,7 +17,7 @@ export default async function handler(
     });
 
     if (!isObjectNullish(urlData)) {
-      res.json({ data: urlData });
+      res.json({ data: 'urlData' });
     } else {
       res.status(400).json({ error: 'URL not found' });
     }
